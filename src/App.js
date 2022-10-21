3ee56975ce3aac83994dc994158c5d9bfef3bfc6
ecaterinaursu <ecaterina.ursu@deutshefintechsolutions.de>
2022-10-21 15:24:28 +0300

    resolved threads

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar/Navbar'
 import Homepage from './screens/home/Homepage'
-import Footer from "./components/Footer";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
