945772647ed5c913f2b42c798528eb995df35d01
ecaterinaursu <ecaterina.ursu@deutshefintechsolutions.de>
2022-10-21 13:28:30 +0300

    Created footer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from './components/Navbar'
 import Homepage from './screens/home/Homepage'
+import Footer from "./components/Footer";
 
 function App() {
-  return(
-      <>
-        <Navbar />
-        <Homepage />
-      </>
-  )
+    return (
+        <>
+            <Navbar/>
+            <Homepage/>
+            <Footer/>
+        </>
+    )
 }
 
 export default App;
