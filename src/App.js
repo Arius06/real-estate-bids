918362902f6aeb7030a3dd3f6c01c1f1108f6f0d
Maxim <maximplamadeala13@gmail.com>
2022-10-24 19:04:18 +0300

    I finished a Contact page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from './components/Navbar'
 import Homepage from './screens/home/Homepage'
+import Contact_page from "./components/contact_page/Contact_page";
 
 function App() {
   return(
       <>
-        <Navbar />
-        <Homepage />
+          <Navbar/>
+          <Homepage/>
       </>
   )
 }
