339661f04ea24952da2954e2b01a9100e3b7743c
Ecaterina Ursu <112877761+ecaterinaursu@users.noreply.github.com>
2022-11-09 16:11:50 +0200

    Added routing to the application (#8)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Footer from "./components/footer/Footer";
 import Homepage from './screens/home/Homepage'
 import DetailsScreen from "./screens/apart-details/DetailsScreen";
 import Contact_page from "./components/contact_page/Contact_page";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Navbar/>
-            <Homepage/>
-            <DetailsScreen/>
-            <Contact_page/>
+            <Routes>
+                <Route path={"/"} element={<Homepage/>}/>
+                <Route path={"/details"} element={<DetailsScreen/>}/>
+                <Route path={"/contact"} element={<Contact_page/>}/>
+            </Routes>
             <Footer/>
-        </>
+        </BrowserRouter>
+
     )
 }
+
 export default App;
